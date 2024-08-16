e334b545dd199357cb459cdbeb6d1e9aa7b3afdd
Björn Daigger <bjoern.daigger@gmail.com>
2024-08-16 13:28:58 +0200

    test with second component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, AboutMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
